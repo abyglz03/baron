8bfa49db8450876da25d88af2a672fd537ed3d54
raglz <rulofw98@gmail.com>
2022-09-28 16:01:05 -0500

    Create conexion.php

---
+++ css/db/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+//creamos la conexcion con la base de datos
+
+/* ---------------------------------------
+
+    * Filename: db.php
+    * Original Author: @rulofw98
+    * Web: http://rulofw98.github.io
+    * Modifications by: @rulofw98
+    * Web: 
+    ------------------------------------*/
+define("DB_HOST", "localhost"); //*DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "comapa"); //*Nombre de la base de datos
+define("DB_USER", "root"); //* Usuario de tu base de datos
+define("DB_PASS", "root");//* Contraseña del usuario de la base de datos
+define("DB_ENCODE", "utf8");//*Codificacion de los caracteres
+define("PRO_NOMBRE", "Baron");//*Nombre del proyecto
+
