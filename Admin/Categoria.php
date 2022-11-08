b15d47c9fe9083571e68f0f72b5c992a8136bf39
abyglz03 <113219663+abyglz03@users.noreply.github.com>
2022-11-07 20:13:34 -0600

    MKOO

--- Admin/Categoria.php
+++ Admin/Categoria.php
@@ -72,37 +72,20 @@ include 'global/DbConnection.php';
     <div class="container-scroller">
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo mr-4" href="index.html"><img src="images/LOGO-BARON-EFECTO.png" class="mr-2" alt="logo" /></a>
-                <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/LOGO-BARON-EFECTO.png" alt="logo" /></a>
-            </div>
-            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-                    <span class="icon-menu"></span>
-                </button>
-
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-                    <a class="dropdown-item">
-                        <i class="ti-settings text-primary"></i>
-                        Settings
-                    </a>
-                    <a class="dropdown-item">
-                        <i class="ti-power-off text-primary"></i>
-                        Logout
-                    </a>
-                </div>
-                </li>
-                <li class="nav-item nav-settings d-none d-lg-flex">
-                    <a class="nav-link" href="#">
-                        <i class="icon-ellipsis"></i>
-                    </a>
-                </li>
-                </ul>
-                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-                    <span class="icon-menu"></span>
-                </button>
-            </div>
-        </nav>
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="pro.html"><img src="images/LOGO-BARON-EFECTO.png" class="mr-2" alt="logo"></a>
+        <a class="navbar-brand brand-logo-mini" href="pro.html"><img src="images/LOGO-BARON-EFECTO.png" alt="logo"></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+       
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
         <!-- partial -->
         <div class="container-fluid page-body-wrapper">
             <!-- partial:partials/_settings-panel.html -->
@@ -281,23 +264,39 @@ include 'global/DbConnection.php';
             <!-- partial -->
             <!-- partial:partials/_sidebar.html -->
             <nav class="sidebar sidebar-offcanvas" id="sidebar">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">
-                            <i class="icon-grid menu-icon"></i>
-                            <span class="menu-title">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <div class="collapse" id="auth">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="categorias.html"> Categorias </a></li>
-                                <li class="nav-item"> <a class="nav-link" href="producto.html"> Producto </a></li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="Producto.php">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Producto</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Usertype.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Tipo de usuario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Categoria.php">
+              <i class="icon-columns menu-icon"></i>
+              <span class="menu-title">Categoria</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="userform.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Usuario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Cliente.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Cliente</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
             <!-- partial -->
             <div class="main-panel">
                 <div class="content-wrapper">
--- Admin/producto.php
+++ Admin/producto.php
@@ -144,56 +144,14 @@ include 'global/DbConnection.php';
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-4" href="index.html"><img src="images/LOGO-BARON-EFECTO.png" class="mr-2" alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo" /></a>
+        <a class="navbar-brand brand-logo mr-5" href="Producto.html"><img src="images/LOGO-BARON-EFECTO.png" class="mr-2" alt="logo"></a>
+        <a class="navbar-brand brand-logo-mini" href="Producto.html"><img src="images/LOGO-BARON-EFECTO.png" alt="logo"></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span class="icon-menu"></span>
         </button>
-        <ul class="navbar-nav mr-lg-2">
-          <li class="nav-item nav-search d-none d-lg-block">
-            <div class="input-group">
-              <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
-                <span class="input-group-text" id="search">
-                  <i class="icon-search"></i>
-                </span>
-              </div>
-              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
-            </div>
-          </li>
-        </ul>
-        <ul class="navbar-nav navbar-nav-right">
-          <li class="nav-item dropdown">
-            <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-              <i class="icon-bell mx-0"></i>
-              <span class="count"></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-              <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
-            </div>
-          </li>
-          <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-              <img src="images/faces/face28.jpg" alt="profile" />
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-              <a class="dropdown-item">
-                <i class="ti-settings text-primary"></i>
-                Settings
-              </a>
-              <a class="dropdown-item">
-                <i class="ti-power-off text-primary"></i>
-                Logout
-              </a>
-            </div>
-          </li>
-          <li class="nav-item nav-settings d-none d-lg-flex">
-            <a class="nav-link" href="#">
-              <i class="icon-ellipsis"></i>
-            </a>
-          </li>
-        </ul>
+       
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
           <span class="icon-menu"></span>
         </button>
@@ -230,12 +188,35 @@ include 'global/DbConnection.php';
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="Producto.php">
               <i class="icon-grid menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
+              <span class="menu-title">Producto</span>
             </a>
           </li>
-
+          <li class="nav-item">
+            <a class="nav-link" href="Usertype.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Tipo de usuario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Categoria.php">
+              <i class="icon-columns menu-icon"></i>
+              <span class="menu-title">Categoria</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="userform.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Usuario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Cliente.php">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Cliente</span>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- partial -->
